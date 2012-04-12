f341d6a8621668ab8a80ea1cc66308cf3b33360b
Marcin Czenko <marcin@everydayproductive.com>
2012-04-12 15:47:57 +0200

    Added controllers for a one-level topics hierarchy with some example groups and topics.

---
+++ AgileToolbox/QATTopicsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  QATTopicsListViewController.h
+//  AgileToolbox
+//
+//  Created by AtrBea on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QATTopicsListViewController : UITableViewController
+
+@end
