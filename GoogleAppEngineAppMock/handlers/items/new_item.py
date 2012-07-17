d155b7f445753551298f1a20dff355df69199f42
Marcin Czenko <marcin@everydayproductive.com>
2012-07-17 15:43:42 +0200

    Acceptance tests restructured slightly, GoogleServerMock updated to support POST JSON

---
+++ GoogleAppEngineAppMock/handlers/items/new_item.py
@@ -0,0 +1,11 @@
+import webapp2
+import json
+
+from models.items import ItemsModel
+
+class NewItem(webapp2.RequestHandler):
+    def post(self):
+        new_json_item = json.loads(self.request.body)
+        ItemsModel.addItem(new_json_item[0]['content'])
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('ADDED')
---
+++ UIAutomation/specs/WalkingSkeleton/PostingNewItemToTheServer.js
@@ -0,0 +1,37 @@
+#import "../../jasmine-uiautomation.js"
+#import "../../jasmine/lib/jasmine-core/jasmine.js"
+#import "../../jasmine-uiautomation-reporter.js"
+
+
+describe("Walking Skeleton", function() {
+
+    var target = UIATarget.localTarget();
+    var mainWindow = target.frontMostApp().mainWindow();
+    
+    function getLabel() {
+        return target.frontMostApp().mainWindow().staticTexts()[0].value();
+    }
+    
+    afterEach(function() {
+        target.frontMostApp().navigationBar().leftButton().tap();
+    });
+
+    it("posts the new item to the server", function() {
+        
+        mainWindow.tableViews()["MenuList"].cells()["Q&A"].tap();
+        
+        expect(mainWindow.tableViews()["Questions"].checkIsValid()).toBe(true);
+        expect(mainWindow.tableViews()["Questions"].cells().length).toEqual(5);
+        
+        target.frontMostApp().navigationBar().buttons()["Add"].tap();
+        mainWindow.textFields()["NewQuestionTextField"].tap();
+        target.frontMostApp().keyboard().typeString("New Question");
+        target.frontMostApp().navigationBar().buttons()["Done"].tap();
+        
+        expect(mainWindow.tableViews()["Questions"].checkIsValid()).toBe(true);
+        expect(mainWindow.tableViews()["Questions"].cells().length).toEqual(6);
+    });
+});
+
+jasmine.getEnv().addReporter(new jasmine.UIAutomation.Reporter());
+jasmine.getEnv().execute();
