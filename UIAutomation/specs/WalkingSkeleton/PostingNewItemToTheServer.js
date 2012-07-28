3ecaea820fe69612627cd31685f5ccc787d13e3d
Marcin Czenko <marcin@everydayproductive.com>
2012-07-29 00:54:48 +0200

    Fixing problem with big capital letters being typed too fast in the simulator. Hey, this should be covered by test, because the test would still pass !!!!!!!

--- UIAutomation/specs/WalkingSkeleton/PostingNewItemToTheServer.js
+++ UIAutomation/specs/WalkingSkeleton/PostingNewItemToTheServer.js
@@ -25,7 +25,23 @@ describe("Walking Skeleton", function() {
         
         target.frontMostApp().navigationBar().buttons()["Add"].tap();
         mainWindow.textFields()["NewQuestionTextField"].tap();
-        target.frontMostApp().keyboard().typeString("New Question");
+        
+        // It seems that the names of the key,change depending on the status of the shift button.
+        // If shift is enabled then the key is called 'N',if shift is not enabled then it's 'n'.
+        // You'll notice as a string is being typed,that the shift button is tapped before an uppercase
+        // letter is typed.Your test is attempting to press the 'N' key before the 'Shift' button has been
+        // pressed.It doesn't affect the first letter of your sentence because the keyboard has shift
+        // enabled for the first letter.
+        //
+        // This also affects typing a lowercase character after an uppercase character:the lowercase
+        // character may be typed whilst the shift button is in the process of being unpressed.
+        //
+        // I use a workaround of typing each letter of the string with separate typeString() methods.
+        // See http://stackoverflow.com/questions/10549046/target-frontmostapp-keyboard-failed-to-locate-key-n
+        target.frontMostApp().keyboard().typeString("N");
+        target.frontMostApp().keyboard().typeString("ew Q");
+        target.frontMostApp().keyboard().typeString("uestion");
+        
         target.frontMostApp().navigationBar().buttons()["Done"].tap();
         
 	    target.delay(1);
